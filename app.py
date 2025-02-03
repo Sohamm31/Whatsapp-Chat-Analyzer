c6d5ce90517bc648a9fa3c9e857fcf6bc241c1fb
Sohamm31 <sohamm3107@gmail.com>
2025-02-03 15:53:20 +0530

    Updated with Busy Users and Bar Graphs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import preprocessor,helper
+import matplotlib.pyplot as plt
 st.sidebar.title("Whatsapp Chat Analyzer")
 
 uploaded_file = st.sidebar.file_uploader("Choose a File")
@@ -15,8 +16,31 @@ if uploaded_file is not None:
     selected_user =  st.sidebar.selectbox("Show Analysis wrt",user_list)
 
     if st.sidebar.button("Show Analysis"):
-        num_messages = helper.fetch_stats(selected_user,df)
+        num_messages,words,num_media_messages,doc_messages = helper.fetch_stats(selected_user,df)
         col1,col2,col3,col4 = st.columns(4)
         with col1:
             st.header("Total Messages")
             st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+        with col3:
+            st.header("Media Shared")
+            st.title(num_media_messages)
+        with col4:
+            pass
+            st.header("Documents Shared")
+            st.title(doc_messages)
+
+        if selected_user == "Overall":
+            st.title("Most Busy Users")
+            x,new_df = helper.most_busy_users(df)
+            fig,ax = plt.subplots()
+            col1,col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index,x.values,color = "blue")
+                plt.xticks(rotation = "vertical")
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
--- helper.py
+++ helper.py
@@ -1,5 +1,21 @@
 def fetch_stats(selected_user,df):
-    if selected_user == "Overall":
-        return df.shape[0]
-    else:
-        return df[df["Username"] == selected_user].shape[0]
+    if selected_user != "Overall":
+        df = df[df["Username"] == selected_user]
+
+    num_messages = df.shape[0]
+    words = []
+    for messages in df["Message"]:
+        words.extend(messages.split())
+
+    num_media_messages = df[df["Message"].str.contains("image omitted|video omitted", na=False)].shape[0]
+
+    doc_messages = df[df["Message"].str.contains("document omitted", na=False)].shape[0]
+
+    return num_messages, len(words),num_media_messages,doc_messages
+
+
+def most_busy_users(df):
+    x = df["Username"].value_counts().head()
+    df = round(df["Username"].value_counts()/df.shape[0]*100,2).reset_index().rename(columns = {"index":"Name","Username":"Percent"})
+    return x,df
+
